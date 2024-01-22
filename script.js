5b4b1c21ac689fd42bda2f191cb71b213f2b9c90
mandalbitayan <140152734+mandalbitayan@users.noreply.github.com>
2024-01-22 18:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //your JS code here. If required.
 function createelement(){
-   // let createh2=document.createElement("h3")
-   // console.log(createh2)
-   const body=document.getElementsByTagName("body")
-   body.textContent="DOM load success";
-	console.log(body.textContent)
-   // body[0].appendChild(createh2);
-}
-createelement()
+    const body=document.getElementsByTagName("body")
+    let createh2=document.createElement("p")
+    console.log(createh2)
+    createh2.textContent="DOM load success";
+    body[0].appendChild(createh2);
+ }
+ createelement()
