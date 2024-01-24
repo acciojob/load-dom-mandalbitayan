8e3b9c762d958454326866ff032eea9e00aee417
mandalbitayan <140152734+mandalbitayan@users.noreply.github.com>
2024-01-24 11:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 //your JS code here. If required.
-function createelement(){
-    const body=document.getElementsByTagName("body")
-    let createh2=document.createElement("p")
-    console.log(createh2)
-    createh2.textContent="DOM load success";
-    body[0].appendChild(createh2);
- }
- createelement()
+document.addEventListner("DOMcontentLoaded",()=>{
+	document.body.innerText="DOM load success";
+})
