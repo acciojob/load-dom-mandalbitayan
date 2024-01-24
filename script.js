cb1c1abe7e13be181b61934cc12b395e295a5d7e
mandalbitayan <140152734+mandalbitayan@users.noreply.github.com>
2024-01-24 11:07:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.addEventListner("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",()=>{
 	document.body.innerText="DOM load success";
 })
